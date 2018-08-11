ee910eebc4dff895f93a4b9a073f49c4c123f2e5
Aby Abraham <abyabrkal@gmail.com>
2018-08-11 11:28:28 +0400

    V12 - Events Handling
    Events Handling

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { getFunName } from '../helpers'
+import { runInThisContext } from 'vm';
+
+/* 
+    If we try to access 'this' from any user defined functions, the value will be undefined.
+    But, we will be able to access 'this' as the 'component' if accessed within React defined functions.
+
+    So, here we need to bind 'this' to the user defined functions (like goToStore). Couple of ways are
+    1.  add a constructor method (with super) and manually bind all user def functions to this.
+            constructor(){
+                super()
+                this.goToStore = this.gotToStore.bind(this)
+            }
+    2.  User defined functions definition needs to be changed to an expression like variable definition (using arrow method)
+        and will be automatically binded to the component in which it is defined.
+            goToStore = event => {
+                console.log(this)   // this will now point to StorePicker
+            }
+*/
+
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef()
+    
+    goToStore = (event) => {
+        // stop form from submitting
+        event.preventDefault();
+
+        // get text from input
+        const storeName = this.myInput.value.value
+
+        // change the page to /store/--name
+        // the React Router passes down functions like history, location, match.
+        // One such history.push() is used to push the url
+        this.props.history.push(`/store/${storeName}`)
+    }
+
     render(){
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name"/>
+                <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
