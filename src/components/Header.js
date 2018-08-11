f15d1b6e4c0d5755b0c8655af70bdedbcc22db21
Aby Abraham <abyabrkal@gmail.com>
2018-08-11 10:20:33 +0400

    Props and Stateless Function component
    Props explored.
    And, Stateless Function components too (render only components).

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+function Header (props) {
+    return (
+        <header className="top">
+            <h1>Catch
+                <span className="ofThe">
+                    <span className="of">Of</span>
+                    <span className="the">The</span>
+                </span> 
+                Day
+            </h1>
+            <h3 className="tagline">
+                <span>{props.tagline}</span>
+            </h3>
+        </header>
+    )
+}
+
+
+
+/* Since Header use only render function and not any others like state or lifecycle methods,
+   this is a stateless function component. So, we can simplify like function above.  
+
 class Header extends React.Component {
     render(){
         return(
@@ -12,11 +34,11 @@ class Header extends React.Component {
                     Day
                 </h1>
                 <h3 className="tagline">
-                    <span>Fresh Seafood Market</span>
+                    <span>{this.props.tagline}</span>
                 </h3>
             </header>
         )
     }
-}
+} */
 
 export default Header;
