7ebef81eee13118bc846705d57f3ec3d8278c4aa
Aby Abraham <abyabrkal@gmail.com>
2018-08-11 10:39:55 +0400

    V9 React Router
    React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-import App from './components/App'
-import StorePicker from './components/StorePicker'
 import './css/style.css'
 
+import Router from './components/Router'
 
-render(<App />, document.querySelector('#main'));
+
+render(<Router />, document.querySelector('#main'));
